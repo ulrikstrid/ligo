08a0d8411badb666cd8ffe69a725f93211cdc088
Georges Dupéron <georges.duperon@nomadic-labs.com>
2019-06-14 14:08:34 +0200

    Fix PWD detection of installer script (WIP, needs testing)

--- scripts/ligo.sh
+++ scripts/ligo.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -e
-if test "x$PWD" = "x"; then
-  echo "Cannot detect the current directory, the environment variable PWD is empty."
-  exit 1
-else
+if test -d "$PWD"; then
   docker run -it -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:next "$@"
+else
+  echo "Cannot detect the current directory."
+  echo "This often happens when the current directory gets renamed or moved."
+  echo "Trying to cd to the desired folder may solve this."
+  exit 1
 fi
 # Do not remove the next line. It is used as an approximate witness that the download of this file was complete. This string should not appear anywhere else in the file.
 # END OF DOWNLOADED FILE
