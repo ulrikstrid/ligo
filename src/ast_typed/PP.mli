bb1912605f5ba490750142ec27601ad55b437794
Pierre-Emmanuel Wulfman <pop@openaliasbox.org>
2019-08-06 10:02:29 +0200

    WIP : ast_type

---
+++ src/ast_typed/PP.mli
@@ -0,0 +1,32 @@
+open Types
+open Format
+
+val list_sep_d : ( formatter -> 'a -> unit ) -> formatter -> 'a list -> unit
+val smap_sep_d : ( formatter -> 'a -> unit ) -> formatter -> 'a Map.String.t -> unit
+
+
+val type_value : formatter -> type_value -> unit
+
+val annotated_expression : formatter -> annotated_expression -> unit
+
+val lambda : formatter -> lambda -> unit
+
+val expression : formatter -> expression -> unit
+
+val value : formatter -> annotated_expression -> unit
+
+val assoc_annotated_expression : formatter -> (ae * ae) -> unit
+
+val literal : formatter -> literal -> unit
+
+val single_record_patch : formatter -> ( string * ae ) -> unit
+
+val matching_variant_case : ( formatter -> 'a  -> unit ) -> formatter -> ( constructor_name * name ) * 'a -> unit
+
+val matching : ( formatter -> 'a -> unit ) -> formatter -> 'a matching -> unit
+
+val pre_access : formatter -> access -> unit
+
+val declaration : formatter -> declaration -> unit
+
+val program : formatter -> program -> unit
