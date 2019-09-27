935dc33e27744ac8063f98a0cfd539807f95c5ca
John David Pressman <jd@jdpressman.com>
2019-09-26 19:44:24 -0700

    Horrible very confused edits so people can tell me what I'm doing wrong

---
+++ src/bin/cli-js.ml
@@ -0,0 +1,98 @@
+open Cmdliner
+open Trace
+open Cli_helpers
+
+let main =
+  let term = Term.(const print_endline $ const "Ligo needs a command. Do ligo --help") in
+  (term , Term.info "ligo")
+
+let source n =
+  let open Arg in
+  let info =
+    let docv = "SOURCE_FILE" in
+    let doc = "$(docv) is the path to the .ligo or .mligo file of the contract." in
+    info ~docv ~doc [] in
+  required @@ pos n (some string) None info
+
+let entry_point n =
+  let open Arg in
+  let info =
+    let docv = "ENTRY_POINT" in
+    let doc = "$(docv) is entry-point that will be compiled." in
+    info ~docv ~doc [] in
+  required @@ pos n (some string) (Some "main") info
+
+let expression purpose n =
+  let open Arg in
+  let docv = purpose ^ "_EXPRESSION" in
+  let doc = "$(docv) is the expression that will be compiled." in
+  let info = info ~docv ~doc [] in
+  required @@ pos n (some string) None info
+
+let syntax =
+  let open Arg in
+  let info =
+    let docv = "SYNTAX" in
+    let doc = "$(docv) is the syntax that will be used. Currently supported syntaxes are \"pascaligo\" and \"cameligo\". By default, the syntax is guessed from the extension (.ligo and .mligo, respectively)." in
+    info ~docv ~doc ["syntax" ; "s"] in
+  value @@ opt string "auto" info
+
+let amount =
+  let open Arg in
+  let info =
+    let docv = "AMOUNT" in
+    let doc = "$(docv) is the amount the dry-run transaction will use." in
+    info ~docv ~doc ["amount"] in
+  value @@ opt string "0" info
+
+let compile_file =
+  let f source entry_point syntax =
+    toplevel @@
+    let%bind contract =
+      trace (simple_info "compiling contract to michelson") @@
+      Ligo.Run.compile_contract_file source entry_point (Syntax_name syntax) in
+    Format.printf "%s\n" contract ;
+    ok ()
+  in
+  let term =
+    Term.(const f $ source 0 $ entry_point 1 $ syntax) in
+  let cmdname = "compile-contract" in
+  let docs = "Subcommand: compile a contract. See `ligo " ^ cmdname ^ " --help' for a list of options specific to this subcommand." in
+  (term , Term.info ~docs cmdname)
+
+let compile_parameter =
+  let f source entry_point expression syntax =
+    toplevel @@
+    let%bind value =
+      trace (simple_error "compile-input") @@
+      Ligo.Run.compile_contract_parameter source entry_point expression (Syntax_name syntax) in
+    Format.printf "%s\n" value;
+    ok ()
+  in
+  let term =
+    Term.(const f $ source 0 $ entry_point 1 $ expression "PARAMETER" 2 $ syntax) in
+  let cmdname = "compile-parameter" in
+  let docs = "Subcommand: compile parameters to a michelson expression. The resulting michelson expression can be passed as an argument in a transaction which calls a contract. See `ligo " ^ cmdname ^ " --help' for a list of options specific to this subcommand." in
+  (term , Term.info ~docs cmdname)
+
+let compile_storage =
+  let f source entry_point expression syntax =
+    toplevel @@
+    let%bind value =
+      trace (simple_error "compile-storage") @@
+      Ligo.Run.compile_contract_storage source entry_point expression (Syntax_name syntax) in
+    Format.printf "%s\n" value;
+    ok ()
+  in
+  let term =
+    Term.(const f $ source 0 $ entry_point 1 $ expression "STORAGE" 2 $ syntax) in
+  let cmdname = "compile-storage" in
+  let docs = "Subcommand: compile an initial storage in ligo syntax to a michelson expression. The resulting michelson expression can be passed as an argument in a transaction which originates a contract. See `ligo " ^ cmdname ^ " --help' for a list of options specific to this subcommand." in
+  (term , Term.info ~docs cmdname)
+
+
+let () = Term.exit @@ Term.eval_choice main [
+    compile_file ;
+    compile_parameter ;
+    compile_storage ;
+  ]
