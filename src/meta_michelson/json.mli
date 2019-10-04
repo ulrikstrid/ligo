a28da82439a11994c1323a9b51f692a9d0703de1
John David Pressman <jd@jdpressman.com>
2019-10-04 12:47:38 -0700

    Add .mli files without changes to make them work again

--- src/meta_michelson/json.mli
+++
@@ -1,3 +0,0 @@
-val force_record : msg:string -> [> `O of 'a] -> 'a
-
-val force_string : msg:string -> [> `String of 'a] -> 'a
--- src/meta_michelson/streams.mli
+++
@@ -1,3 +0,0 @@
-val read_file : string -> string
-
-val read_lines : string -> string list
