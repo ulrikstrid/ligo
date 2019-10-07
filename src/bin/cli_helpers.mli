0e0eb6d99ae38ea8dfd5fdfa51a9f4d5b8b70771
Pierre-Emmanuel Wulfman <pop@openaliasbox.org>
2019-10-07 11:23:44 +0200

    update and build correctly

--- src/bin/cli_helpers.mli
+++ src/bin/cli_helpers.mli
@@ -1,3 +1,3 @@
 open Trace
 
-val toplevel : unit result -> unit
+val toplevel : display_format : string -> string result -> unit
--- src/main/run_simplified.mli
+++
@@ -1,6 +0,0 @@
-open Trace
-val run_simplityped : ?options:Proto_alpha_utils.Memory_proto_alpha.options -> ?debug_mini_c:bool -> ?debug_michelson:bool 
--> Ast_typed.program -> string -> Ast_simplified.expression -> Ast_simplified.expression result
-
-val evaluate_simplityped : ?options:Proto_alpha_utils.Memory_proto_alpha.options ->  ?debug_mini_c:bool -> ?debug_michelson:bool -> Ast_typed.program
--> string -> Ast_simplified.expression result 
