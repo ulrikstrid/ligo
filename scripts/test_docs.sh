17231a872d6708565fc55cc57d4e72ef5d9b25d9
Sander Spies <sandermail@gmail.com>
2019-10-22 22:25:46 +0200

    fml

--- scripts/test_docs.sh
+++ scripts/test_docs.sh
@@ -3,4 +3,8 @@ cd gitlab-pages/docs
 dune clean 
 dune build --verbose 
 opam info mdx
+which ocaml-mdx
+which ocaml-mdx-test
+ocaml-mdx --help=plain
+ocaml-mdx-test --help=plain
 dune runtest --verbose
