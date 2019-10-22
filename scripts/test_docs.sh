de2c6d4ca023e2426f0507e140bb3431894c9992
Sander Spies <sandermail@gmail.com>
2019-10-22 21:22:59 +0200

    CI testing

--- scripts/test_docs.sh
+++ scripts/test_docs.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-cd gitlab-pages/docs && dune clean && dune build --verbose && dune runtest --verbose
+cd gitlab-pages/docs 
+dune clean 
+dune build --verbose 
+opam info mdx
+dune runtest --verbose
