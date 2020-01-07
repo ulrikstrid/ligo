064c3d65e77ddb6327f40e78ecab1035bd9d9805
Sander Spies <sandermail@gmail.com>
2020-01-07 11:02:29 +0100

    Remove duplicate test.

--- src/bin/expect_tests/lexer_tests.ml
+++ src/bin/expect_tests/lexer_tests.ml
@@ -32,22 +32,6 @@ ligo: lexer error: The string starting here is interrupted by a line break.
 
  |} ];
 
-run_ligo_bad [ "compile-contract" ; "../../test/lexer/broken_string.religo" ; "main" ] ;
-  [%expect {| 
-../../test/lexer/broken_string.religo:1:9: warning: missing terminating '"' character [-Winvalid-pp-token]
-let a = "broken
-        ^
-../../test/lexer/broken_string.religo:4:6: warning: missing terminating '"' character [-Winvalid-pp-token]
-lines";
-     ^
-2 warnings generated.
-ligo: lexer error: The string starting here is interrupted by a line break.
-      Hint: Remove the break, close the string before or insert a backslash.
-       {"parser_loc":"in file \"broken_string.religo\", line 1, characters 8-9"}
-      
-
- |} ];
-
 run_ligo_bad [ "compile-contract" ; "../../test/lexer/negative_byte_sequence.mligo" ; "main" ] ;
   [%expect {| 
 ligo: lexer error: Negative byte sequence.
