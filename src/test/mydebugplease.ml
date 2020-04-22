de88a27608567ea62f1718501348fa00c676200c
Suzanne Dupéron <ligo@suzanne.soy>
2020-04-22 20:03:15 +0100

    DEBUG : DO NOT MERGE Inspect the monad error result of the standalone test

--- src/test/mydebugplease.ml
+++ src/test/mydebugplease.ml
@@ -1,4 +1,8 @@
 Printexc.record_backtrace true ;
 Printf.printf "Start of mydebugplease executable\n%!";
-let _ = Typer_tests.TestExpressions.lambda () in
+let test_result = Typer_tests.TestExpressions.lambda () in
+let test (x : unit Trace.result) : unit = match x with
+| Ok (() , _annotation_thunk) -> ()
+| Error err -> failwith (Yojson.Basic.to_string @@ err ()) in
+let () = test test_result in
 Printf.printf "End of mydebugplease executable\n%!"
