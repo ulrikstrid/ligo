62137b4510c363f30f4ca544a3f4f7f074f99e8c
Suzanne Dupéron <ligo@suzanne.soy>
2020-08-10 17:59:30 +0100

    WIP2

--- src/test/heuristic_tc_fundep_tests.ml
+++ src/test/heuristic_tc_fundep_tests.ml
@@ -78,45 +78,121 @@ let tests1 restrict = [
 )  ;    
 ]
 
-let (<?) ca cb =
-  if ca = 0 then cb () else ca
+module Compare_cleaned = struct
+  type 'a tree = Leaf of 'a | Pair of 'a tree * 'a tree
+    
+  let (<?) ca cb =
+    match ca with
+    | Some t1 ->
+      (match cb () with
+       | Some t2 -> Some (Pair (t1, t2))
+       | None -> None)
+    | None -> None
 
-let compare_cleaned_constraints (expected : constraints) (actual : constraints) =
-  Stdlib.failwith "todo"
+  let list ~compare expected actual =
+    List.fold_left2 (function
+        | None -> fun _ _ -> None
+        | Some t1 -> fun a b ->
+          match compare a b with
+          | None -> None
+          | Some t2 -> Some (Pair (t1, t2)))
+      expected actual
 
-let compare_cleaned_p_forall (expected : p_forall) (actual : p_forall) =
-  Stdlib.failwith "todo"
-
-let compare_cleaned_type_value_ (expected : type_value_) (actual : type_value_) =
-  (* We compare type variables during a later pass. *)
-  match expected, actual with
-  | (Ast_typed.Types.P_forall   { binder=_; constraints=a1; body=a2 } , Ast_typed.Types.P_forall   { binder=_; constraints=b1; body=b2 }) ->
-    compare_cleaned_constraints a1 b1 <? fun () ->
-      compare_cleaned_p_forall a2 b2
-  | (Ast_typed.Types.P_variable _ , Ast_typed.Types.P_variable _) -> ignore (a,b); 0
-  | (Ast_typed.Types.P_constant a , Ast_typed.Types.P_constant b) -> ??
-  | (Ast_typed.Types.P_apply    a , Ast_typed.Types.P_apply    b) -> ??
-  | (Ast_typed.Types.P_row      a, Ast_typed.Types.P_row       b) -> ??
-  | (a, b) ->
-    let different = Ast_typed.Compare_generic.type_value_ a b in
-    assert (different != 0); different
-
-let compare_cleaned_type_value (expected : type_value) (actual : type_value) =
-  let { tsrc=_; t=a1 } = expected in
-  let { tsrc=_; t=b1 } = actual in
-  compare_cleaned_type_value_ a1 b1
+  type 'a cmp = 'a -> 'a -> type_variable tree option
   
-let compare_cleaned_tc_allowed (expected : tc_allowed) (actual : tc_allowed) =
-  List.compare ~compare:compare_cleaned_type_value expected actual
+  let rec c_equation : c_equation cmp = fun expected actual ->
+    let { aval=a1; bval=a2 } = expected in
+    let { aval=b1; bval=b2 } = actual in
+    type_value a1 b1 <? fun () -> type_value a2 b2
 
-let compare_cleaned_typeclass (expected : typeclass) (actual : typeclass) =
-  List.compare ~compare:compare_cleaned_tc_allowed expected actual
+  and tc_allowed : tc_allowed cmp = fun expected actual ->
+    List.compare ~compare:type_value expected actual
+
+  and typeclass : typeclass cmp = fun expected actual ->
+    List.compare ~compare:tc_allowed expected actual
+
+  and tc_args : tc_args cmp = fun expected actual ->
+    List.compare ~compare:type_value expected actual
+
+  and c_typeclass : c_typeclass cmp = fun expected actual ->
+    let { tc_args=a1; typeclass=a2 } = expected in
+    let { tc_args=b1; typeclass=b2 } = actual in
+    tc_args a1 b1 <? fun () -> typeclass a2 b2
+
+  and c_access_label : c_access_label cmp = fun expected actual ->
+    let { c_access_label_tval=a1; accessor=a2; c_access_label_tvar=a3 } = expected in
+    let { c_access_label_tval=b1; accessor=b2; c_access_label_tvar=b3 } = actual in
+    type_value a1 b1 <? fun () -> Ast_typed.Compare_generic.label a2 b2 <? fun () -> type_variable a3 b3
+
+  and type_constaint_ : type_constraint_ cmp = fun expected actual ->
+    match expected, actual with
+    | Ast_typed.Types.C_equation     a , Ast_typed.Types.C_equation     b -> c_equation a b
+    | Ast_typed.Types.C_typeclass    a , Ast_typed.Types.C_typeclass    b -> c_typeclass a b
+    | Ast_typed.Types.C_access_label a , Ast_typed.Types.C_access_label b -> c_access_label a b
+    | (a, b) ->
+      let different = Ast_typed.Compare_generic.type_constraint_ a b in
+      assert (different != 0); different
+  and type_constraint : type_constraint cmp = fun expected actual ->
+    let { reason=_; c=a1 } = expected in
+    let { reason=_; c=b1 } = actual in
+    type_constaint_ a1 b1
+
+  and p_constraints : p_constraints cmp = fun expected actual ->
+    List.compare ~compare:type_constraint expected actual
+
+  and type_variable : type_variable cmp = fun expected actual ->
+    (* We compare type variables during a later pass. *)
+    ignore (expected,actual); 0
+
+  and p_forall : p_forall cmp = fun expected actual ->
+    let { binder=_; constraints=a1; body=a2 } = expected in
+    let { binder=_; constraints=b1; body=b2 } = actual in
+    p_constraints a1 b1 <? fun () ->
+      type_value a2 b2
+
+  and p_ctor_args : p_ctor_args cmp = fun expected actual ->
+    List.compare ~compare:type_value expected actual
+
+  and p_constant : p_constant cmp = fun expected actual ->
+    let { p_ctor_tag = a1; p_ctor_args = a2 } = expected in
+    let { p_ctor_tag = b1; p_ctor_args = b2 } = actual in
+    Ast_typed.Compare_generic.constant_tag a1 b1 <? fun () ->
+      p_ctor_args a2 b2
+
+  and p_apply : p_apply cmp = fun expected actual ->
+    let { tf=a1; targ=a2 } = expected in
+    let { tf=b1; targ=b2 } = actual in
+    type_value a1 b1 <? fun () ->
+      type_value a2 b2
   
-let compare_cleaned_c_typeclass_simpl (expected : c_typeclass_simpl) (actual : c_typeclass_simpl) =
-  let { reason_typeclass_simpl=_; tc=a1; args=a2 } = expected in
-  let { reason_typeclass_simpl=_; tc=b1; args=b2 } = actual in
-  compare_cleaned_typeclass a1 b1 <? fun () ->
-  Ast_typed.Compare_generic.type_variable_list a2 b2
+  and p_row : p_row cmp = fun expected actual ->
+    let { p_row_tag = a1; p_row_args = a2 } = expected in
+    let { p_row_tag = b1; p_row_args = b2 } = actual in
+    Ast_typed.Compare_generic.row_tag a1 b1 <? fun () ->
+      LMap.compare type_value a2 b2
+
+  and type_value_ : type_value_ cmp = fun expected actual ->
+    match expected, actual with
+    | (Ast_typed.Types.P_forall   a , Ast_typed.Types.P_forall   b) -> p_forall a b
+    | (Ast_typed.Types.P_variable a , Ast_typed.Types.P_variable b) -> type_variable a b
+    | (Ast_typed.Types.P_constant a , Ast_typed.Types.P_constant b) -> p_constant a b
+    | (Ast_typed.Types.P_apply    a , Ast_typed.Types.P_apply    b) -> p_apply a b
+    | (Ast_typed.Types.P_row      a , Ast_typed.Types.P_row      b) -> p_row a b
+    | (a, b) ->
+      let different = Ast_typed.Compare_generic.type_value_ a b in
+      assert (different != 0); different
+
+  and type_value : type_value cmp = fun expected actual ->
+    let { tsrc=_; t=a1 } = expected in
+    let { tsrc=_; t=b1 } = actual in
+    type_value_ a1 b1
+
+  and c_typeclass_simpl : c_typeclass_simpl cmp = fun expected actual ->
+    let { reason_typeclass_simpl=_; tc=a1; args=a2 } = expected in
+    let { reason_typeclass_simpl=_; tc=b1; args=b2 } = actual in
+    typeclass a1 b1 <? fun () ->
+      Ast_typed.Compare_generic.type_variable_list a2 b2
+end
 
 let test'
     name
