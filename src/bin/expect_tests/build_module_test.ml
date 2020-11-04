344c53d2b3a903f564c58dd91c3fe2e217b56c23
Pierre-Emmanuel Wulfman <pierre.emmanuel.wulfman@gmail.com>
2020-11-04 16:03:28 +0100

    testing

--- src/bin/expect_tests/build_module_test.ml
+++ src/bin/expect_tests/build_module_test.ml
@@ -4,7 +4,39 @@ let contract basename =
   "../../test/contracts/build/" ^ basename
 
 let%expect_test _ =
-  run_ligo_bad [ "compile-contract" ; contract "cycle_A.mligo" ; "main" ] ;
+  run_ligo_good [ "compile-contract" ; contract "B.mligo" ; "f" ] ;
+  [%expect{|
+    { parameter unit ;
+      storage int ;
+      code { PUSH int 1 ; PUSH int 1 ; DIG 2 ; CDR ; ADD ; ADD ; NIL operation ; PAIR } } |}]
+
+let%expect_test _ =
+  run_ligo_good [ "print-ast-typed" ; contract "D.mligo" ] ;
+  [%expect {|
+    const toto = E.toto
+    const main = lambda (#3) return let s = #3.1 in let p = #3.0 in let s = ADD(ADD(p ,
+    s) ,
+    toto) in ( LIST_EMPTY() , s ) |}]
+
+let%expect_test _ =
+  run_ligo_good [ "compile-contract" ; contract "D.mligo"; "main" ] ;
+  [%expect {|
+    { parameter int ;
+      storage int ;
+      code { PUSH int 10 ;
+             SWAP ;
+             DUP ;
+             DUG 2 ;
+             CDR ;
+             DIG 2 ;
+             CAR ;
+             ADD ;
+             ADD ;
+             NIL operation ;
+             PAIR } } |}]
+
+let%expect_test _ =
+  run_ligo_bad [ "print-ast-typed" ; contract "cycle_A.mligo" ] ;
   [%expect {|
     Dependency cycle detected :
      `-- ../../test/contracts/build/cycle_A.mligo
