3d68ca6fdd1020d41d162b6a2156828c1454c198
Pierre-Emmanuel Wulfman <pierre.emmanuel.wulfman@gmail.com>
2020-11-05 14:02:01 +0100

    fix bug with module env not being update; block module in module untill better fix

--- src/build/build.ml
+++ src/build/build.ml
@@ -59,6 +59,8 @@ let add_modules_in_env env deps =
 
 let aggregate_contract order_deps asts_typed =
   let aggregate_deps ast_typed file_set deps_lst =
+    (* This bit generate vars to be used in several place
+    This may be unecessary*)
     let make_vars set (file_name,module_name) =
       let module_var = Location.wrap @@ Var.of_name module_name in
       let file_var,set = match SMap.find_opt file_name set with
@@ -71,24 +73,32 @@ let aggregate_contract order_deps asts_typed =
       (set),(file_var,module_var)
     in
     let file_set,deps_lst = List.fold_map_acc make_vars file_set deps_lst in
+    (* In case of main requires A who requires B, this will put record B inside record A in main.
+      Since we don't update the type of A, this lead to errors in the generated Mini_c
+
     let aux (file_name,module_name) ast_typed =
       let expr = Ast_typed.(make_e @@ E_variable file_name) @@ Ast_typed.t_unit () in
       (Location.wrap @@ Ast_typed.Declaration_constant {binder=module_name;expr;inline=true})
       :: ast_typed
     in
     let ast_typed = List.fold_right aux deps_lst ast_typed in
-    ast_typed,file_set
+    *)
+    ast_typed,file_set,deps_lst
   in
-  let aux (_,(file_set,contracts)) (file_name, (_,_,_, deps_lst)) =
+  (* Recursively add deps in all deps *)
+  let aux (_,(file_set,_,contracts)) (file_name, (_,_,_, deps_lst)) =
     let%bind (Ast_typed.Program_Fully_Typed ast_typed,_) =
       trace_option (corner_case ~loc:__LOC__ "Fail to find typed module") @@
       SMap.find_opt file_name asts_typed in
-    let contract,file_vars = aggregate_deps ast_typed file_set deps_lst in
+    let contract,file_vars,deps_list = aggregate_deps ast_typed file_set deps_lst in
     let contracts = SMap.add file_name contract contracts in
-    ok @@ (contract,(file_vars,contracts))
+    ok @@ (contract,(file_vars,deps_list,contracts))
   in
-  let%bind contract,(file_set,contracts)   = bind_fold_list aux ([],(SMap.empty,SMap.empty)) order_deps in
-  let add_header contracts contract file_name file_var =
+  let%bind contract,(file_set,deps_list,contracts)   = bind_fold_list aux ([],(SMap.empty,[],SMap.empty)) order_deps in
+  let add_header contracts contract (file_name,_) =
+    let%bind file_var =
+      trace_option (corner_case ~loc:__LOC__ "Fail to find file_var") @@
+      SMap.find_opt file_name file_set in
     let%bind ast_typed =
       trace_option (corner_case ~loc:__LOC__ "Fail to find aggregated module") @@
       SMap.find_opt file_name contracts in
@@ -113,53 +123,46 @@ let aggregate_contract order_deps asts_typed =
     (Location.wrap @@ Ast_typed.Declaration_constant {binder=file_var;expr=record;inline=false})
     :: contract
   in
-  let%bind contract   = bind_fold_smap (add_header contracts) (ok @@ contract) file_set in
+  (* Add the called module at the beginning of the file *)
+    let aux (file_name,module_name) ast_typed =
+      let expr = Ast_typed.(make_e @@ E_variable file_name) @@ Ast_typed.t_unit () in
+      (Location.wrap @@ Ast_typed.Declaration_constant {binder=module_name;expr;inline=true})
+      :: ast_typed
+    in
+  let contract   = List.fold_right aux deps_list contract in
+  (* Add all dependency at the beginning of the file *)
+  let%bind contract   = bind_fold_list (add_header contracts) contract @@
+    List.tl @@ List.rev order_deps in
   ok @@ Ast_typed.Program_Fully_Typed contract
+
+let type_file_with_dep ~options ~protocol_version asts_typed (file_name, (meta,form,c_unit,deps)) =
+  let%bind ast_core = trace compiler_error @@ Compile.Utils.to_core ~options ~meta c_unit file_name in
+  let aux (file_name,module_name) =
+    let%bind ast_typed =
+      trace_option (corner_case ~loc:__LOC__
+      "File typed before dependency. The build system is broken, contact the devs")
+      @@ SMap.find_opt file_name asts_typed
+    in
+    ok @@ (module_name, ast_typed)
+  in
+  let%bind deps = bind_map_list aux deps in
+  let%bind init_env   = trace compiler_error @@ Compile.Helpers.get_initial_env protocol_version in
+  let init_env = add_modules_in_env init_env deps in
+  let%bind ast_typed,ast_typed_env,_ = trace compiler_error @@ Compile.Of_core.compile ~typer_switch:options.typer_switch ~init_env form ast_core in
+  ok @@ SMap.add file_name (ast_typed,ast_typed_env) asts_typed
 
 let type_contract : options:Compiler_options.t -> string -> Compile.Of_core.form -> _ -> file_name -> (_, _) result =
   fun ~options syntax entry_point protocol_version file_name ->
     let%bind deps = dependency_graph syntax ~options entry_point file_name in
     let%bind order_deps = solve_graph deps file_name in
-    let type_file_with_dep asts_typed (file_name, (meta,form,c_unit,deps)) =
-      let%bind ast_core = trace compiler_error @@ Compile.Utils.to_core ~options ~meta c_unit file_name in
-      let aux (file_name,module_name) =
-        let%bind ast_typed =
-          trace_option (corner_case ~loc:__LOC__
-          "File typed before dependency. The build system is broken, contact the devs")
-          @@ SMap.find_opt file_name asts_typed
-        in
-        ok @@ (module_name, ast_typed)
-      in
-      let%bind deps = bind_map_list aux deps in
-      let%bind init_env   = trace compiler_error @@ Compile.Helpers.get_initial_env protocol_version in
-      let init_env = add_modules_in_env init_env deps in
-      let%bind ast_typed,ast_typed_env,_ = trace compiler_error @@ Compile.Of_core.compile ~typer_switch:options.typer_switch ~init_env form ast_core in
-      ok @@ SMap.add file_name (ast_typed,ast_typed_env) asts_typed
-    in
-    let%bind asts_typed = bind_fold_list type_file_with_dep (SMap.empty) order_deps in
+    let%bind asts_typed = bind_fold_list (type_file_with_dep ~options ~protocol_version) (SMap.empty) order_deps in
     ok @@ fst @@ SMap.find file_name asts_typed
 
 let build_mini_c : options:Compiler_options.t -> string -> _ -> _ -> file_name -> (_, _) result =
   fun ~options syntax entry_point protocol_version file_name ->
     let%bind deps = dependency_graph syntax ~options entry_point file_name in
     let%bind order_deps = solve_graph deps file_name in
-    let type_file_with_dep asts_typed (file_name, (meta,form,c_unit,deps)) =
-      let%bind ast_core = trace compiler_error @@ Compile.Utils.to_core ~options ~meta c_unit file_name in
-      let aux (file_name,module_name) =
-        let%bind ast_typed =
-          trace_option (corner_case ~loc:__LOC__
-          "File typed before dependency. The build system is broken, contact the devs")
-          @@ SMap.find_opt file_name asts_typed
-        in
-        ok @@ (module_name, ast_typed)
-      in
-      let%bind deps = bind_map_list aux deps in
-      let%bind init_env   = trace compiler_error @@ Compile.Helpers.get_initial_env protocol_version in
-      let init_env = add_modules_in_env init_env deps in
-      let%bind ast_typed,ast_typed_env,_ = trace compiler_error @@ Compile.Of_core.compile ~typer_switch:options.typer_switch ~init_env form ast_core in
-      ok @@ SMap.add file_name (ast_typed,ast_typed_env) asts_typed
-    in
-    let%bind asts_typed = bind_fold_list type_file_with_dep (SMap.empty) order_deps in
+    let%bind asts_typed = bind_fold_list (type_file_with_dep ~options ~protocol_version) (SMap.empty) order_deps in
     let%bind contract = aggregate_contract order_deps asts_typed in
     let%bind mini_c     = trace compiler_error @@ Compile.Of_typed.compile @@ contract in
     ok @@ mini_c
