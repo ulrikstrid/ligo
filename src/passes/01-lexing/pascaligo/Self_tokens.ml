0ecad014ec32d66651a6e1a27d5ea66573b935dc
Christian Rinderknecht <Christian.Rinderknecht@tezcore.com>
2020-12-20 17:37:14 +0100

    Self_tokens now includes a pass from lexical units to tokens.

--- src/passes/01-lexing/pascaligo/Self_tokens.ml
+++ src/passes/01-lexing/pascaligo/Self_tokens.ml
@@ -1,8 +1,26 @@
+(* This module implements a filter on the lexical units of PascaLIGO
+   and produces tokens to be consumed by the parser. *)
+
+(* Vendor dependencies *)
+
+module Core = LexerLib.Core
+
+(* Signature *)
+
 module type S =
   sig
     type token
-    val process : token list -> token list
+    type lex_unit = token Core.lex_unit
+
+    val filter : lex_unit list -> token list
   end
 
 type token = Token.t
-let process (tokens: token list) = tokens
+type lex_unit = token Core.lex_unit
+
+let filter lex_units =
+  let open Core in
+  let apply tokens = function
+    Token token -> token::tokens
+  | Markup _ | Directive _ -> tokens
+  in List.fold_left apply [] lex_units |> List.rev
--- src/passes/01-lexing/shared/Self_tokens.mli
+++ src/passes/01-lexing/shared/Self_tokens.mli
@@ -1,6 +1,13 @@
+(* Vendor dependencies *)
+
+module Core = LexerLib.Core
+
+(* Signature *)
+
 module type S =
   sig
     type token
+    type lex_unit = token Core.lex_unit
 
-    val process : token list -> token list
+    val filter : lex_unit list -> token list
   end
