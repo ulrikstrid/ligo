bb345e6e77d845e00de0816492f2b98cb3572a6b
Suzanne Soy <ligo@suzanne.soy>
2021-02-07 13:55:55 +0000

    WIP, builds, tests pass

--- src/passes/10-typing/typer-new/heuristic_break_ctor.mli
+++ src/passes/10-typing/typer-new/heuristic_break_ctor.mli
@@ -9,24 +9,24 @@ val heuristic : <
 
 val selector_ : ( type_variable -> type_variable ) -> type_constraint_simpl -> type_variable GroupedByVariable.t -> output_break_ctor list
 
-(* module type Indexes = sig
- *   val grouped_by_variable : type_variable GroupedByVariable.t
- * end *)
+module type Indexes = sig
+  val grouped_by_variable : type_variable GroupedByVariable.t
+end
 
-(* module H : sig
- *   open Typer.Errors
- *   type selector_output
- *   val heuristic_name : string
- *   val selector : (type_variable -> type_variable) -> type_constraint_simpl ->
- *     < grouped_by_variable : type_variable GroupedByVariable.t;
- *       .. >
- *     -> selector_output list
- *   val alias_selector : type_variable -> type_variable ->
- *     < grouped_by_variable : type_variable GroupedByVariable.t; .. >
- *     -> selector_output list
- *   val get_referenced_constraints : selector_output -> type_constraint_simpl list
- *   val propagator : (selector_output, typer_error) propagator
- *   val printer : Format.formatter -> output_break_ctor -> unit
- *   val printer_json : selector_output -> Yojson.Safe.t
- *   val comparator : selector_output -> selector_output -> int
- * end *)
+module H : sig
+  open Typer_common.Errors
+  type selector_output
+  val heuristic_name : string
+  val selector : (type_variable -> type_variable) -> type_constraint_simpl ->
+    < grouped_by_variable : type_variable GroupedByVariable.t;
+      .. >
+    -> selector_output list
+  val alias_selector : type_variable -> type_variable ->
+    < grouped_by_variable : type_variable GroupedByVariable.t; .. >
+    -> selector_output list
+  val get_referenced_constraints : selector_output -> type_constraint_simpl list
+  val propagator : (selector_output, typer_error) propagator
+  val printer : Format.formatter -> output_break_ctor -> unit
+  val printer_json : selector_output -> Yojson.Safe.t
+  val comparator : selector_output -> selector_output -> int
+end
