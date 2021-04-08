6ebf67afc7cfebd155ced87fba32573accca9287
Gino Imbrailo <ginoimbrailo@gmail.com>
2021-04-08 11:55:13 -0700

    chore: Removed compile method from ligo-compiler.ts

--- nix/ligo-editor.nix
+++ nix/ligo-editor.nix
@@ -70,6 +70,7 @@ let
   ligo-editor = writeShellScriptBin "ligo-editor" ''
     set -e
     LIGO_CMD=${../ligo} \
+    LIGO_CMD2=${../ligo} \
     STATIC_ASSETS=${client} \
     DATA_DIR=/tmp \
     ${nodejs}/bin/node ${server}/node_modules/server/dist/src/index.js
