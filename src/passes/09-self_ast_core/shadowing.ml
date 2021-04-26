30d6fcc63750816b5e743f8bcca639497d6f77fb
E. Rivas <8037031-er433@users.noreply.gitlab.com>
2021-04-26 15:33:22 -0300

    Add some more cases

--- src/passes/09-self_ast_core/shadowing.ml
+++ src/passes/09-self_ast_core/shadowing.ml
@@ -27,11 +27,19 @@ let rec shadowing_match_case : expression_variable list -> (expression, type_exp
   shadowing_expr (vars_in_pattern @ vars) body
 and shadowing_expr : expression_variable list -> expression -> (unit, _) result = fun vars expr ->
   match expr.expression_content with
+  | E_literal _ | E_constant _ | E_variable _ -> ok @@ ()
   | E_lambda {binder={var;_};_} when in_vars var vars ->
      fail @@ shadowing var
+  | E_application {lamb;args} ->
+     let%bind _ = shadowing_expr vars lamb in
+     shadowing_expr vars args
   | E_lambda {binder={var;attributes;_};result} when is_not_shadowable attributes ->
      shadowing_expr (var :: vars) result
   | E_lambda {result;_} ->
+     shadowing_expr vars result
+  | E_recursive {fun_name;_} when in_vars fun_name vars ->
+     fail @@ shadowing fun_name
+  | E_recursive {lambda={result;_};_} ->
      shadowing_expr vars result
   | E_let_in {let_binder={var;_};_} when in_vars var vars ->
      fail @@ shadowing var
@@ -41,23 +49,28 @@ and shadowing_expr : expression_variable list -> expression -> (unit, _) result 
   | E_let_in {rhs;let_result;_} ->
      let%bind _ = shadowing_expr vars rhs in
      shadowing_expr vars let_result
-  | E_application {lamb;args} ->
-     let%bind _ = shadowing_expr vars lamb in
-     shadowing_expr vars args
   | E_type_in {let_result;_} ->
      shadowing_expr vars let_result
   | E_mod_in {let_result;_} ->
      shadowing_expr vars let_result
+  | E_mod_alias {result;_} ->
+     shadowing_expr vars result
+  | E_raw_code _ ->
+     ok @@ ()
   | E_constructor {element;_} ->
      shadowing_expr vars element
   | E_matching {matchee;cases} ->
      let%bind _ = shadowing_expr vars matchee in
      let%bind _ = bind_map_list (shadowing_match_case vars) cases in
      ok @@ ()
-  | E_record _rows ->
+  | E_record rows ->
+     let%bind _ = Helpers.bind_map_lmap (fun expr -> shadowing_expr vars expr) rows in
      ok @@ ()
-  | E_literal _ | E_constant _ | E_variable _ -> ok @@ ()
-  | _ -> ok @@ ()
+  | E_record_accessor _ -> ok @@ ()
+  | E_record_update _ -> ok @@ ()
+  | E_ascription {anno_expr} ->
+     shadowing_expr vars anno_expr
+  | E_module_accessor _ -> ok @@ ()
 
 let rec shadowing_map_module : module' -> (module', _) result = fun m' ->
   let _self = shadowing_map_module in
@@ -69,25 +82,3 @@ let rec shadowing_map_module : module' -> (module', _) result = fun m' ->
   in
   let%bind _ = bind_map_list (bind_map_location aux) m' in
   ok @@ m'
-
-  (* let update_annotations annots c =
-   *   List.fold_right (fun a r -> update_annotation a r) annots c in
-   * let aux = fun (x : declaration) ->
-   *   match x with
-   *   | Declaration_constant {expr ; _} -> (
-   *     let defuse,_ = defuse_neutral in
-   *     let _,unused = defuse_of_expr defuse expr in
-   *     let warn_var v =
-   *       `Self_ast_typed_warning_unused
-   *         (Location.get_location v, Format.asprintf "%a" Var.pp (Location.unwrap v)) in
-   *     update_annotations (List.map warn_var unused) @@
-   *       ok @@ ()
-   *   )
-   *   | Declaration_type _ -> ok @@ ()
-   *   | Declaration_module {module_} ->
-   *     let%bind _ = self module_ in
-   *     ok @@ ()
-   *   | Module_alias _ -> ok @@ ()
-   * in
-   * let%bind _ = bind_map_list (bind_map_location aux) p in
-   * ok @@ (Module_Fully_Typed p) *)
